8f51dc46e1f56e91c217c255427a355706eab714
ladybugtju <ines.bahri@yahoo.com>
2015-05-12 01:47:12 -0700

    First commit

---
+++ db/migrate/20150511190252_create_jobs.rb
@@ -0,0 +1,13 @@
+class CreateJobs < ActiveRecord::Migration
+
+
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+    	t.text :title
+		t.text :description
+	    t.timestamps
+    end
+  end
+end
+
